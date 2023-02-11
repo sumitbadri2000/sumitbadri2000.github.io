a0c5caca07ae3477b9796d8196fcca1fe1fc01bc
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-02-11 11:13:46 +0530

    portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,29 @@
-import "./Navbar.css";
+import styled from "styled-components";
+import Burger from "./Burger";
+
+const Nav = styled.nav`
+  font-family: Arial, Helvetica, sans-serif;
+  height: 55px;
+  font-size:18px;
+  border-bottom: 1px solid #f1f1f1;
+  padding: 0 40px;
+  display: flex;
+  color:white;
+  justify-content: space-between;
+  background-color:#808080;
+  .logo {
+    padding: 20px 0;
+  }
+`;
+
 function Navbar() {
   return (
-    <div className="navbar">
-      <div>
-        <a href="#home">
-          SUMIT<span>BADRI</span>
-        </a>
+    <Nav>
+      <div className="logo">
+        SUMIT<span>BADRI</span>
       </div>
-      <div>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#footer">Contact</a>
-        {/* <a
-          class="navbar-link"
-          href="https://drive.google.com/uc?export=download&id=1BiWFYySamZukJAk_cZrY26f4h8Hs2Ncr"
-          target="blank">
-          Resume
-        </a> */}
-        <a
-          class="navbar-link"
-          href="https://drive.google.com/uc?export=download&id=1BiWFYySamZukJAk_cZrY26f4h8Hs2Ncr"
-          onClick={() =>
-            window.open(
-              "https://drive.google.com/file/d/1BiWFYySamZukJAk_cZrY26f4h8Hs2Ncr/view?usp=sharing"
-            )
-          }>
-          Resume
-        </a>
-      </div>
-    </div>
+      <Burger />
+    </Nav>
   );
 }
 
