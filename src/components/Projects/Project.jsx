1ed7686ab0b38e0c77fb372fcc872c598508c218
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-02-28 15:14:18 +0530

    add

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -22,21 +22,7 @@ function Project() {
             </p>
             <div className="tech">
               <p>Tech Stack - </p>
-              <img
-                className="images"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpngGRjYX1ca7qAADU3K6eGLj7ShQE3L2otdzfryl_Y9Ht2QRoQKYQbsXd36XIxMbYOw0&usqp=CAU"
-                alt="html"
-              />
-              <img
-                className="images"
-                src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
-                alt="css"
-              />
-              <img
-                className="images"
-                src="https://images.squarespace-cdn.com/content/v1/5df7d8d44b9a7a49b2e475ac/1576526689110-QJM10YTHVZYU7Z7D2YLE/js-logo.png"
-                alt="js"
-              />
+              <p className="color">HTML | CSS | JavaScript</p>
             </div>
             <div className="website">
               <a
@@ -65,26 +51,7 @@ function Project() {
             </p>
             <div className="tech">
               <p>Tech Stack - </p>
-              <img
-                className="images"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpngGRjYX1ca7qAADU3K6eGLj7ShQE3L2otdzfryl_Y9Ht2QRoQKYQbsXd36XIxMbYOw0&usqp=CAU"
-                alt="html"
-              />
-              <img
-                className="images"
-                src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
-                alt="css"
-              />
-              <img
-                className="images"
-                src="https://images.squarespace-cdn.com/content/v1/5df7d8d44b9a7a49b2e475ac/1576526689110-QJM10YTHVZYU7Z7D2YLE/js-logo.png"
-                alt="js"
-              />
-              <img
-                className="images"
-                src="https://miro.medium.com/max/400/1*tX7DC-HhKR2blBSbf3xvtQ.jpeg"
-                alt="json_server"
-              />
+              <p className="color">HTML| CSS | JavaScript</p>
             </div>
             <div className="website">
               <a
@@ -114,16 +81,7 @@ function Project() {
             </p>
             <div className="tech">
               <p>Tech Stack - </p>
-              <img
-                className="images"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpngGRjYX1ca7qAADU3K6eGLj7ShQE3L2otdzfryl_Y9Ht2QRoQKYQbsXd36XIxMbYOw0&usqp=CAU"
-                alt="html"
-              />
-              <img
-                className="images"
-                src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
-                alt="css"
-              />
+            <p className="color">HTML | CSS</p>
             </div>
             <div className="website">
               <a
@@ -153,36 +111,7 @@ function Project() {
             </p>
             <div className="tech">
               <p>Tech Stack - </p>
-              <img
-                className="images"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpngGRjYX1ca7qAADU3K6eGLj7ShQE3L2otdzfryl_Y9Ht2QRoQKYQbsXd36XIxMbYOw0&usqp=CAU"
-                alt="html"
-              />
-              <img
-                className="images"
-                src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
-                alt="css"
-              />
-              <img
-                className="images"
-                src="https://images.squarespace-cdn.com/content/v1/5df7d8d44b9a7a49b2e475ac/1576526689110-QJM10YTHVZYU7Z7D2YLE/js-logo.png"
-                alt="js"
-              />
-              <img
-                className="images"
-                src="https://intellyx.com/wp-content/uploads/2019/08/Render-cloud-intellyx-BC-logo.png"
-                alt="json_server"
-              />
-              <img
-                className="images"
-                src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png"
-                alt="React"
-              />
-              <img
-                className="images"
-                src="https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png"
-                alt="React"
-              />
+              <p className="color">HTML |CSS |JavaScript |React |Chakra-UI</p>
             </div>
             <div className="website">
               <a href="https://cute-elf-8e4002.netlify.app/" target="blank">
